546a5baf03cf37232156dfdb22dcd98eba066bbb
rylandking <ryland@users.noreply.github.com>
2019-06-18 13:15:01 -0600

    Initial Selly Attempt 6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = required('body-parser');
 const app = express();
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({
   extended: true
