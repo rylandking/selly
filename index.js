a2128f81900da0e4017b2f49b2e526b5a322ddea
rylandking <ryland@users.noreply.github.com>
2019-06-18 13:42:32 -0600

    Testing with port 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
   res.status(200).send('Hello world');
 });
 
-app.listen(process.env.PORT || 5000, function() {
+app.listen(3000, function() {
   console.log('Yep! Listening on port 5000');
 })
 
